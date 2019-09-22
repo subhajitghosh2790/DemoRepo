9293072f8d463ab4863b924d0bdb76267ddf09ea
subhajit <ghoshsjk@gmail.com>
2019-09-22 23:13:39 +0100

    added search page and modified login page

--- src/main/java/com/qa/pages/LoginPage.java
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -5,6 +5,12 @@ public class LoginPage {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Thos is LoginPage");
+		
 	}
 
+	
+	public void checkChangeinGit() {
+		System.out.println("my first change in GIT");
+	}
+	
 }
---
+++ src/main/java/com/qa/pages/SearchPage.java
@@ -0,0 +1,12 @@
+package com.qa.pages;
+
+public class SearchPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("This is for Search Page");
+	}
+
+}
